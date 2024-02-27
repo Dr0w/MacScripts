5582fc7904191fe3ffb101f5125961936222e254
Yevhenii B <drow.public@gmail.com>
2024-02-27 13:08:20 -0500

    Add support for flexible terminal size
    Now will output properly after terminal resize

--- watch.sh
+++ watch.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 # usage: watch.sh -n <sleep_duration> "<your_command>"
 
+function update_terminal_size {
+  cols=$(tput cols)
+  rows=$(tput lines)
+}
+
+function truncate_command {
+  echo "$1" | cut -c1-80
+}
+
 if [ "$#" -lt 3 ]; then
   echo "Usage: $0 -n <sleep_duration> \"<your_command>\""
   exit 1
 fi
+
+trap update_terminal_size SIGWINCH
 
 while getopts "n:" opt; do
   case $opt in
@@ -24,7 +35,16 @@ shift $((OPTIND-1))
 while :
 do
   clear
-  date
+  
+  update_terminal_size
+
+  truncated_command=$(truncate_command "$1")
+  echo -n "Every $sleep_duration sec.: $truncated_command"
+
+  tput cup 0 $((cols - 28))
+  echo "$(date)"
+  
   eval "$1"
+  
   sleep "$sleep_duration"
 done
